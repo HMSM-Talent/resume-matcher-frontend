e1d08f4f60dcb4e90e639ff7ebab3a03913e5cdd
MDK175 <mitudas_de@yahoo.com.au>
2025-06-02 14:49:49 +1000

    example update

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -4,7 +4,19 @@ function HistoryPage() {
   return (
     <div>
       <h1>History Page</h1>
-    </div>
+      <div>
+        <h2>History</h2>
+        <p>hi, this is the histry page</p>
+      </div>
+
+      <div>
+        <h2>History</h2>
+        <p>hi, this is the histry page</p>
+      </div>
+
+      
+
+      </div>
   );
 }
 
